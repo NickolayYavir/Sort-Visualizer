9a641e2569e30ca38287970247f8c35dc0f572fe
Mykola Yavir <kolyayavir4@gmail.com>
2023-02-24 18:30:45 +0200

    Change UI
    -Delete main canvas
    -Swap bottom frame to canvas

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ from config import *
 
 window = Tk()
 window.title("Sorting visualizer")
+window['bg'] = "#18122B"
 window.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
 window.resizable(width=False, height=False)
 
-canvas = Canvas(window, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, bg='#18122B')
-canvas.pack()
-
-ui_frame = LabelFrame(canvas, text="UI", bg='#18122B', fg='#635985')
+ui_frame = LabelFrame(window, text="UI", bg='#18122B', fg='#635985')
 ui_frame.place(relx = 0.05, rely = 0.05,  relwidth=0.9, relheight=0.15)
 
 element_shuffle_button = Button(ui_frame, text="Shuffle", font=40, bg='#393053', fg="#635985")
@@ -32,8 +30,8 @@ sorting_start_button.place(relx=0.8, rely=0.25, relheight=0.5, relwidth=0.18)
 
 
 
-output_frame = Frame(window, bg='black')
-output_frame.place(relx = 0.05, rely = 0.3,  relwidth=0.9, relheight=0.65)
+output_canvas = Canvas(window, bg='black')
+output_canvas.place(relx = 0.05, rely = 0.3,  relwidth=0.9, relheight=0.65)
 
 
 window.mainloop()
