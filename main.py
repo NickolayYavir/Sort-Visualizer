b00c10779b69e704a54a53b26c2d3c9b46e30847
Mykola Yavir <kolyayavir4@gmail.com>
2023-02-24 21:27:25 +0200

    Add button disable/enable logic

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ from element import ElementsManager
 from sort import Sort
 
 def shuffle_elements():
+    sorting_start_button["state"] = "normal"
+    sorting_start_button["bg"] = COLOR_DARK_2
     Em.createElements()
 
 def start_sorting():
     current_value = sorting_combobox.get()
-    if current_value == Sort.BUBBLE_SORT.value: Em.bubble_sort()
-
-
+    if current_value:
+        element_shuffle_button["state"] = "disabled"
+        sorting_start_button["state"] = "disabled"
+        element_shuffle_button["bg"] = COLOR_LIGHT_1
+        sorting_start_button["bg"] = COLOR_LIGHT_1
+        if current_value == Sort.BUBBLE_SORT.value: Em.bubble_sort()
+        element_shuffle_button["state"] = "normal"
+        element_shuffle_button["bg"] = COLOR_DARK_2
+    
 
 window = Tk()
 window.title("Sorting visualizer")
@@ -22,14 +30,14 @@ window.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
 window.resizable(width=False, height=False)
 
 ui_frame = LabelFrame(window, text="UI", bg=COLOR_DARK_1 , fg=COLOR_LIGHT_2)
-element_shuffle_button = Button(ui_frame, text="Shuffle", font=40, bg=COLOR_DARK_2, fg=COLOR_LIGHT_2, command=shuffle_elements)
+element_shuffle_button = Button(ui_frame, text="Shuffle", font=40, bg=COLOR_DARK_2, fg="White", command=shuffle_elements)
 sorting_label = Label(ui_frame, text="Choose sorting algorithm:", font=36, bg=COLOR_DARK_1 , fg=COLOR_LIGHT_2)
 style= ttk.Style()
 style.theme_use('clam')
 style.configure("TCombobox", fieldbackground=COLOR_LIGHT_2, background= COLOR_DARK_2)
 sorting_combobox = ttk.Combobox(ui_frame)
 sorting_combobox['values'] = [e.value for e in Sort]
-sorting_start_button = Button(ui_frame, text="Start", font=40, bg=COLOR_DARK_2, fg=COLOR_LIGHT_2, command=start_sorting)
+sorting_start_button = Button(ui_frame, text="Start", font=40, bg=COLOR_DARK_2, fg="White", command=start_sorting)
 output_canvas = Canvas(window, bg='black')
 
 ui_frame.place(relx = 0.05, rely = 0.05,  relwidth=0.9, relheight=0.15)
