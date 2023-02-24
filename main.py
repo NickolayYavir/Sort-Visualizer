6a643be1e080f9db56c14a88856cdd90bccec119
Mykola Yavir <kolyayavir4@gmail.com>
2023-02-24 19:12:24 +0200

    Add shuffle button functional

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from tkinter import ttk
 import numpy as np
 
 from config import *
+from element import ElementFactory
+
+def shuffle_elements():
+    ElementFactory(output_canvas).createElements()
+
 
 window = Tk()
 window.title("Sorting visualizer")
@@ -13,7 +18,7 @@ window.resizable(width=False, height=False)
 ui_frame = LabelFrame(window, text="UI", bg='#18122B', fg='#635985')
 ui_frame.place(relx = 0.05, rely = 0.05,  relwidth=0.9, relheight=0.15)
 
-element_shuffle_button = Button(ui_frame, text="Shuffle", font=40, bg='#393053', fg="#635985")
+element_shuffle_button = Button(ui_frame, text="Shuffle", font=40, bg='#393053', fg="#635985", command=shuffle_elements)
 element_shuffle_button.place(relx=0.02, rely=0.25, relheight=0.5, relwidth=0.18)
 
 sorting_label = Label(ui_frame, text="Choose sorting algorithm:", font=36, bg="#18122B", fg="#635985")
@@ -27,8 +32,6 @@ sorting_combobox.place(relx=0.48, rely = 0.25, relheight=0.5, relwidth=0.28)
 
 sorting_start_button = Button(ui_frame, text="Start", font=40, bg='#393053', fg='#635985')
 sorting_start_button.place(relx=0.8, rely=0.25, relheight=0.5, relwidth=0.18)
-
-
 
 output_canvas = Canvas(window, bg='black')
 output_canvas.place(relx = 0.05, rely = 0.3,  relwidth=0.9, relheight=0.65)
