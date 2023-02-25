232d1f7940198d89165ee1aa8c5ef418da351b3f
Mykola Yavir <kolyayavir4@gmail.com>
2023-02-25 03:11:37 +0200

    UI behavior change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import ttk
-import numpy as np
 
 from config import *
 from element import ElementsManager
@@ -9,12 +8,14 @@ from sort import Sort
 def shuffle_elements():
     Em.set_element_quantity(element_quantity_scale.get())
     sorting_start_button.configure(text="Start", state='normal', bg=COLOR_DARK_2)
+    sorting_delay_scale['state'] = 'normal'
+    sorting_combobox['state'] = 'readonly'
+    
     Em.createElements()
 
 def stop_sorting():
     Em.change_sorting_state()
     sorting_start_button.configure(text="Start", command=start_sorting, state='disabled', bg=COLOR_LIGHT_1)
-
 
 def start_sorting():
     Em.set_sotring_delay(sorting_delay_scale.get())
@@ -24,18 +25,17 @@ def start_sorting():
         element_quantity_scale['state'] = 'disabled'
         sorting_delay_scale['state'] = 'disabled'
         element_shuffle_button["state"] = "disabled"
+        sorting_combobox['state'] = 'disable'
         sorting_start_button.configure(text="Stop", command=stop_sorting) 
         element_shuffle_button["bg"] = COLOR_LIGHT_1
         sorting_start_button["bg"] = COLOR_LIGHT_1
         if sorting_algorithm == Sort.BUBBLE_SORT.value: Em.bubble_sort()
         if sorting_algorithm == Sort.INSERTION_SORT.value: Em.insertion_sort()
         sorting_start_button.configure(text="Start", command=start_sorting, state='disabled', bg=COLOR_LIGHT_1)
-        element_quantity_scale['state'] = 'active'
-        sorting_delay_scale['state'] = 'active'
+        element_quantity_scale['state'] = 'normal'
         element_shuffle_button.config(state='normal', bg=COLOR_DARK_2)
 
     
-
 window = Tk()
 window.title("Sorting visualizer")
 window['bg'] = COLOR_DARK_1 
@@ -48,8 +48,9 @@ sorting_label = Label(ui_frame, text="Choose sorting algorithm:", font=36, bg=CO
 style= ttk.Style()
 style.theme_use('clam')
 style.configure("TCombobox", fieldbackground=COLOR_LIGHT_2, background= COLOR_DARK_2)
-sorting_combobox = ttk.Combobox(ui_frame)
+sorting_combobox = ttk.Combobox(ui_frame, state='readonly')
 sorting_combobox['values'] = [e.value for e in Sort]
+sorting_combobox.current(0)
 sorting_start_button = Button(ui_frame, text="Start", font=40, bg=COLOR_DARK_2, fg="White", command=start_sorting)
 element_quantity_scale = Scale(ui_frame, from_=50, to=450, orient=HORIZONTAL, bg=COLOR_DARK_2, fg=COLOR_LIGHT_2)
 sorting_delay_scale = Scale(ui_frame, from_=0.0, to=1.0, digits=3, resolution=0.01, orient=HORIZONTAL, bg=COLOR_DARK_2, fg=COLOR_LIGHT_2)
--- sort.py
+++ sort.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 class Sort(Enum):
     BUBBLE_SORT = 'Bubble sort'
-    SELECTION_SORT = 'Selection sort'
+    # SELECTION_SORT = 'Selection sort'
     INSERTION_SORT = 'Insertion sort'
-    HEAP_SORT = 'Heap sort'
-    QUIK_SORT = 'Quik sort'
-    MERGE_SORT = 'Mearge sort'
+    # HEAP_SORT = 'Heap sort'
+    # QUIK_SORT = 'Quik sort'
+    # MERGE_SORT = 'Mearge sort'
